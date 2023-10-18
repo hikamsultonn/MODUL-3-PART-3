490c603beb0dd34168b1a7d3abd2436c27915a5e
Ahmad Sulton <sulhisy15@gmail.com>
2023-10-18 09:52:39 +0700

    first-commit

---
+++ app/src/main/java/com/example/mod33/Fragment2.kt
@@ -0,0 +1,21 @@
+package com.example.mod33
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+class Fragment2 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        inflater.inflate(R.layout.fragment_2, container, false)
+        return inflater.inflate(R.layout.fragment_2, container, false)
+    }
+
+}
